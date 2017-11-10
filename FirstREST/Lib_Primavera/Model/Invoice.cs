5882e564f353cc388bb018a92e4bf4d290cfef86
Marcelo Ferreira <pount@sapo.pt>
2017-11-10 16:24:01 +0000

    added Invoice.cs file

---
+++ FirstREST/Lib_Primavera/Model/Invoice.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+
+    public class Invoice
+    {
+
+        public string invoice
+        {
+            get;
+            set;
+        }
+    }
+}
