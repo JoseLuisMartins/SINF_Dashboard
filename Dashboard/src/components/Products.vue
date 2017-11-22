3b4aff9e0e8416ac44955e0b351bd318defce425
Marcelo Ferreira <pount@sapo.pt>
2017-11-22 21:18:06 +0000

    some improvements on Inventory

--- Dashboard/src/components/Products.vue
+++ Dashboard/src/components/Products.vue
@@ -1,17 +1,114 @@
 <template>
-  <v-container>
-    <v-data-table
-          v-bind:headers="headers"
-          :items="items"
-          hide-actions
-          class="elevation-1"
-      >
-      <template slot="items" scope="props">
-        <td>{{ props.item.CodArtigo }}</td>
-        <td class="text-xs-right">{{ props.item.DescArtigo }}</td>
-        <td class="text-xs-right">{{ props.item.STKAtual }}</td>
-      </template>
-    </v-data-table>
+  <v-container mt-5 grid-list-xs>
+
+    <v-layout>
+      <v-flex mb-4 d-flex sm6 offset-sm3 xs12>
+        <v-card>
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs12 sm6>
+                <v-menu
+                  lazy
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  full-width
+                  v-model="menu"
+                  offset-y
+                  :nudge-right="40"
+                  max-width="290px"
+                  max-height="600px"
+                >
+
+                  <v-text-field
+                    slot="activator"
+                    v-model="dateBegin"
+                    prepend-icon="event"
+                    readonly
+                    label="Begin Date"
+                  ></v-text-field>
+                  
+                  <v-date-picker v-model="dateBegin" no-title scrollable actions>
+                    <template  slot-scope="{save, cancel}">
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn flat color="primary" @click="cancel"> Cancel </v-btn>
+                        <v-btn flat color="primary" @click="save"> Ok </v-btn>
+                      </v-card-actions>
+                    </template>
+                  </v-date-picker>
+                </v-menu>
+              </v-flex>
+
+              <v-flex xs12 sm6>
+                <v-menu
+                  lazy
+                  :clonse-on-content-click="false"
+                  transition="scale-transition"
+                  full-width
+                  :nudge-right="40"
+                  max-width="290px"
+                  max-height="600px"
+                >
+
+                  <v-text-field
+                    slot="activator"
+                    v-model="dateEnd"
+                    prepend-icon="event"
+                    readonly
+                    label="End Date"
+                  ></v-text-field>
+                  
+                  <v-date-picker v-model="dateEnd" no-title scrollable actions>
+                    <template slot-scope="{save, cancel}">
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn flat color="primary" @click="cancel"> Cancel </v-btn>
+                        <v-btn flat color="primary" @click="save"> Ok </v-btn>
+                      </v-card-actions>
+                    </template>
+                  </v-date-picker>
+
+                </v-menu>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex d-flex sm12 md12>
+        <v-card>
+          <v-card-title class="pb-0">
+            <div class="headline"> Products </div>
+            <v-spacer></v-spacer>
+            <v-text-field
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+              v-model="search_1"
+            ></v-text-field>
+          </v-card-title>
+          <v-card-text>
+            <v-data-table
+              v-bind:headers="headers"
+              :items="items"
+              hide-actions
+              class="elevation-1"
+            >
+              <template slot="items" scope="props">
+                <td>{{ props.item.CodArtigo }}</td>
+                <td class="text-xs-right">{{ props.item.DescArtigo }}</td>
+                <td class="text-xs-right">{{ props.item.STKAtual }}</td>
+              </template>
+            </v-data-table>
+            <span>TOTAL = {{inventoryValue}} €</span>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
   </v-container>
 </template>
 
@@ -21,20 +118,19 @@ import Products from '@/services/Products'
 export default {
   data () {
     return {
+      inventoryValue: 200,
       pagination: {
         sortBy: 'Codigo'
       },
       selected: [],
       headers: [
-        {
-          text: 'Codigo',
-          align: 'left',
-          value: 'CodArtigo'
-        },
+        { text: 'Codigo', value: 'CodArtigo', align: 'left' },
         { text: 'Descrição', value: 'DescArtigo' },
         { text: 'STKAtual', value: 'STKAtual' }
       ],
-      items: []
+      items: [],
+      dateBegin: null,
+      dateEnd: null
     }
   },
   methods: {
@@ -52,6 +148,11 @@ export default {
     }
   },
   mounted: function () {
+    let currentYear = new Date().getFullYear()
+    this.dateEnd = `${currentYear}-01-01`
+    currentYear -= 1
+    this.dateBegin = `${currentYear}-01-01`
+
     this.$nextTick(async () => {
       const res = await Products.all()
       this.items = res.data
